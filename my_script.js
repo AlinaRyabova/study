4c31489557a1fd20b9b34900f3a2597824a10d51
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2025-02-08 14:23:23 +0200

    study

---
+++ my_script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log("Jacob Mercer");
+console.log(26);
+
+const age = 20;
+let username;
+
+// username = "Poly";
+
+console.log(age);
+
+console.log(username);
+let isstatus = "true";
+console.log(typeof isstatus);
+
+const x = 8;
+const y = 7;
+console.log(x + y);
