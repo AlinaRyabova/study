188572fb9e03f6d6aa366791b08948d6e77d2607
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2025-02-10 11:20:50 +0200

    study

--- my_script.js
+++ my_script.js
@@ -1,19 +1,143 @@
-"use strict";
+// console.log("Jacob Mercer");
+// console.log(26);
 
-console.log("Jacob Mercer");
-console.log(26);
+// const age = 20;
+// let username;
 
-const age = 20;
-let username;
+// // username = "Poly";
 
-// username = "Poly";
+// console.log(age);
 
-console.log(age);
+// console.log(username);
+// let isstatus = "true";
+// console.log(typeof isstatus);
 
-console.log(username);
-let isstatus = "true";
-console.log(typeof isstatus);
+// const x = 8;
+// const y = 7;
+// console.log(x + y);
 
-const x = 8;
-const y = 7;
-console.log(x + y);
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// pricePerItem += 1500;
+
+// console.log(productName[1]);
+// console.log("productName", productName[5]);
+// console.log("productName :>> ", productName[9]);
+// console.log("productName", productName[11]);
+
+// console.log(productName.length);
+// let topSpeed = 160;
+// let distance = 617.54;
+// let login = "mango935";
+// let isOnline = true;
+// let isAdmin = false;
+
+// let pricePerItem = 3500;
+// let orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log("false" + null);
+// let username = "Poly";
+// username[3] = "a";
+// console.log("username", username);
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log("message", message);
+// const username = "Poly";
+// const message = `Username ${username} is ${username.length} characters long`;
+// console.log("message", message);
+
+// const courseTopic = "JavaScript essentials";
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[20];
+// console.log(courseTopicLength);
+// console.log(firstElement);
+// console.log(lastElement);
+// console.log(1 === "1");
+// console.log(0 == false);
+// console.log(Number(true));
+// console.log(Number(null));
+// console.log(Number.parseInt("5px"));
+// console.log(Math.floor(1.2));
+// console.log(Math.ceil(3.3));
+// console.log(Math.round(4.7));
+// console.log(Math.min(10, 30, 60, 65, 70, 90));
+// console.log(Math.max(10, 30, 60, 65, 70, 90));
+// console.log(Math.random());
+// console.log((5.98765).toFixed(2));
+// let age = 20;
+
+// let isAdult = age >= 22;
+
+// console.log(isAdult);
+
+// let correctPassword = "fghj";
+// let userPassword = "fghj";
+// const isValid = userPassword === correctPassword;
+// console.log(isValid);
+// const value = "25.8px";
+// const numerical = parseFloat(value);
+// console.log(numerical);
+// function multiply(x, y, z) {
+//   console.log(`Result: ${x * y * z}`);
+// }
+
+// multiply(2, 3, 4);
+// function multiply(x, y, z) {
+//   return x * y * z;
+// }
+// const result = multiply(2, 3, 5);
+// console.log(result);
+// function makeMessage(username) {
+//   console.log(`Hello ${username}`);
+// }
+
+// makeMessage("Jacob");
+// console.log("A");
+
+// function logStuff() {
+//   console.log("B");
+// }
+
+// console.log("C");
+
+// logStuff();
+
+// console.log("D");
+// function sayHi(params) {
+//   console.log("Hello, this is my first function!");
+// }
+// sayHi();
+// function add(a, b, c) {
+//   const result = a + b + c;
+//   console.log(`"Addition result equals ${result}"`);
+// }
+// add(2, 5, 2);
+// function makeMessage(name, price) {
+//   return `You picked ${name}, price per item is ${price} credits`;
+// }
+// console.log(makeMessage("Radar", 6150));
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+//   const total = orderedQuantity * pricePerItem;
+//   return total;
+// }
+// console.log(calculateTotalPrice(5, 100));
